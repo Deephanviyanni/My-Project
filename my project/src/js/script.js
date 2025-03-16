93770ae2044bf284877917bb7bc6a9998ecfd19e
Deephanviyanni <148992111+Deephanviyanni@users.noreply.github.com>
2025-03-16 20:58:41 +0530

    Delete my project/src directory

--- my project/src/js/script.js
+++
@@ -1,9 +0,0 @@
-const toTop = document.querySelector(".back-top");
-
-window.addEventListener("scroll", () => {
-  if (pageYOffset > 75) {
-    toTop.classList.add("active");
-  } else {
-    toTop.classList.remove("active");
-  }
-});
