78fc61f4bff6569ee38fe2ce98e0b26f21e770cb
Deephanviyanni <148992111+Deephanviyanni@users.noreply.github.com>
2025-03-16 21:01:20 +0530

    Add files via upload

---
+++ my project/src/js/script.js
@@ -0,0 +1,9 @@
+const toTop = document.querySelector(".back-top");
+
+window.addEventListener("scroll", () => {
+  if (pageYOffset > 75) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+});
